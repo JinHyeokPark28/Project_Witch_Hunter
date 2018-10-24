2a07bd0e2dad1dea832c5b12f8ba1a8b661e915e
JinHyeokPark28 <42726179+JinHyeokPark28@users.noreply.github.com>
2018-10-24 21:24:14 +0900

    Add Scripts

---
+++ Witch_Hunter/Assets/Scripts/Monster/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour, IMonster {
+	#region Private Variable
+	
+	#endregion
+
+	#region Public Variable
+	#endregion
+
+	#region Private Method
+	public void Recon(){
+
+	}
+	public void Attack(){
+
+	}
+	public void Chase(){
+
+	}
+	#endregion
+
+	#region Public Method
+	#endregion
+
+}
